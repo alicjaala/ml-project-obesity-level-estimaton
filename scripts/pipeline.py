cb4efde63b43779691d0d6849787701d2d536a12
Alicja Rembisz <153197178+alicjaala@users.noreply.github.com>
2025-11-06 15:22:42 +0100

    Refactor pipeline.py

--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -8,7 +8,6 @@ from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, accuracy_score
 
-# --- 1. Wczytanie danych ---
 data = pd.read_csv('data/obesity_data.csv')
 
 selected_cols = [
@@ -21,10 +20,8 @@ data = data[selected_cols]
 X = data.drop("NObeyesdad", axis=1)
 y = data["NObeyesdad"]
 
-# --- 2. Podział danych 80% trening / 20% test ---
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
-# --- 3. Przetwarzanie danych ---
 numerical_cols = X.select_dtypes(include=['int64', 'float64']).columns
 categorical_cols = X.select_dtypes(include=['object', 'category']).columns
 
@@ -43,22 +40,18 @@ preprocessor = ColumnTransformer([
     ('cat', categorical_transformer, categorical_cols)
 ])
 
-# --- 4. Model ---
 model = Pipeline([
     ('preprocessor', preprocessor),
     ('classifier', LogisticRegression(max_iter=1000))
 ])
 
-# --- 5. Trening i ewaluacja ---
 print("\n=============== REGRESJA LOGISTYCZNA ===============")
 
 model.fit(X_train, y_train)
 
-# Ewaluacja na zbiorze treningowym
 y_train_pred = model.predict(X_train)
 train_acc = accuracy_score(y_train, y_train_pred)
 
-# Ewaluacja na zbiorze testowym
 y_test_pred = model.predict(X_test)
 test_acc = accuracy_score(y_test, y_test_pred)
 
@@ -66,3 +59,4 @@ print(f"\nTrain Accuracy: {train_acc:.4f}")
 print(f"Test Accuracy:  {test_acc:.4f}")
 print("\nClassification Report (Test):")
 print(classification_report(y_test, y_test_pred))
+
